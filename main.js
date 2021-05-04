ddb66cc139e6b551ed23d3bb2a7543b656852443
Godoy <j.godoysoares@gmail.com>
2021-05-03 21:13:15 -0300

    variaveis e arrays

---
+++ main.js
@@ -0,0 +1,37 @@
+var nome = 'Godoy';
+var n1 = 5;
+var n2 = 3;
+var frase = 'Japão é o melhor time do mundo'
+
+//alert(nome + ' tem ' + idade + ' anos');
+
+//alert(idade + idade2);
+
+//console.log(n1 * n2);
+//console.log(frase);
+//console.log(frase.replace('Japão', 'Brasil'));
+//console.log(frase.toUpperCase());
+//console.log(frase.toLowerCase());
+
+
+// --- ARRAY ---
+
+var lista = ['maça', 'pera', 'laranja'];
+lista.push('uva');
+//lista.pop();
+
+//console.log(lista.length);
+//console.log(lista);
+//console.log(lista.reverse());
+//console.log(lista.toString());
+//console.log(lista.join(' - '))
+
+/*
+var fruta = {nome: 'maça', cor: 'vermelha'}
+console.log(fruta.nome);
+*/
+
+
+var frutas = [{nome: 'maça', cor: 'vermelha'}, {nome: 'uva', cor: 'roxa'}]
+console.log(frutas);
+
