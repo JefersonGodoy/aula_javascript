f5c8d4c570c54a1a52517330e8e5f3c67c434e06
Godoy <j.godoysoares@gmail.com>
2021-05-04 22:44:18 -0300

    fim

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var nome = 'Godoy';
+/*var nome = 'Godoy';
 var n1 = 5;
 var n2 = 3;
-var frase = 'Japão é o melhor time do mundo'
+var frase = 'Japão é o melhor time do mundo'*/
 
 //alert(nome + ' tem ' + idade + ' anos');
 
@@ -16,10 +16,10 @@ var frase = 'Japão é o melhor time do mundo'
 
 // --- ARRAY ---
 
-var lista = ['maça', 'pera', 'laranja'];
+/*var lista = ['maça', 'pera', 'laranja'];
 lista.push('uva');
 //lista.pop();
-
+*/
 //console.log(lista.length);
 //console.log(lista);
 //console.log(lista.reverse());
@@ -32,8 +32,8 @@ console.log(fruta.nome);
 */
 
 
-var frutas = [{nome: 'maça', cor: 'vermelha'}, {nome: 'uva', cor: 'roxa'}]
-console.log(frutas);
+/*var frutas = [{nome: 'maça', cor: 'vermelha'}, {nome: 'uva', cor: 'roxa'}]
+console.log(frutas);*/
 
 
 // --- CONDIÇÕES ---
@@ -58,8 +58,78 @@ while(count <= 5 ) {
 }
 */
 
+/*
 var count;
 
-for(count = 1; count <=5; count ++) {
+for(count = 0; count <=5; count ++) {
     alert(count);
 }
+*/
+
+/*
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+*/
+
+// --- FUNÇÕES ---
+
+/*
+function soma(n1, n2) {
+    return n1 + n2;
+}
+
+alert(soma(5, 10));
+*/
+
+/*
+function validaIdade(idade) {
+    var validar;
+    if(idade >= 18) {
+        validar = true;
+    } else {
+        validar = false;
+    }
+    
+    return validar;
+}
+
+var idade = prompt('Qual sua idade?');
+console.log(validaIdade(idade));
+*/
+
+
+
+// --- Manipulando objetos da página ---
+
+/*
+function clicou() {
+    //alert('Obrigado por clicar');
+    document.getElementById('agradecimento').innerHTML = '<b>Obrigado por clicar!!!!<b>';
+    //console.log(document.getElementById('agradecimento'));
+}
+
+function redirecionar() {
+    window.open('https://www.cursoemvideo.com');
+   //window.location.href = 'https://www.cursoemvideo.com';
+}
+
+function trocar(elemento) {
+    //document.getElementById('mousemove').innerHTML = 'Obrigado por passar o mouse!!!'
+    elemento.innerHTML = 'Obrigado por passar o mouse!!!';
+}
+
+function voltar(elemento) {
+   // document.getElementById('mousemove').innerHTML = 'Passe o mouse aqui!';
+   elemento.innerHTML = 'Passe o mouse aqui!';
+}
+
+function load() {
+    alert('Página carregada');
+}
+
+function funcaoChange(elemento) {
+    console.log(elemento.value);
+}
+*/
