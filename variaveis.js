77361d0a6746370fee6dab8c9a2e707b66a7c626
Godoy <j.godoysoares@gmail.com>
2021-05-07 13:12:09 -0300

    tentando

---
+++ variaveis.js
@@ -0,0 +1,16 @@
+var colab = 'Fulano';
+
+//console.log(colab);
+
+function colabo() {
+    if(colab == 'Fulano') {
+        console.log(colab);
+    } else {
+
+        console.log('Não sei que é');
+    }
+
+    return;
+}
+
+console.log(colabo);
